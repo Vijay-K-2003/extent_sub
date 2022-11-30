2ad8ce8dd07d92e6b1593ca01728a11988d40871
Vijay Karanjkar <vijaykaranjkar2003@gmail.com>
2022-11-30 21:52:16 +0530

    :sparkles: Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,17 @@ import Profile from './components/Profile/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <div className="Background">
+    <div className="app">
+      <div className="background">
         <Sidenav />
-        <div className="MainContent">
-          <div className="Header">
-            <Metamask hash={"0x0445.....3E0716"}/>
-            <Profile />
+        <div className="main-content">
+          <div className="main-content__wrapper">
+            <div className="main-content__wrapper__header">
+              <Metamask hash={"0x0445.....3E0716"}/>
+              <Profile fullname={"Nobuhara"} username={"noburockshere"} />
+            </div>
           </div>
+          
         </div>
       </div>
     </div>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import defaultProfile from "./../../assets/defaultprofile.jpeg";
+import "./Profile.scss";
 
-const Profile = () => {
-  return (
-    <div></div>
-  );
+interface ProfileProps {
+    fullname: string;
+    username: string;
+}
+
+const Profile = (props: ProfileProps) => {
+    const {fullname, username} = props;
+    return (
+        <div className="profile">
+            <div className="profile__wrapper">
+                <div className="profile__wrapper--info">
+                    <div className="profile__wrapper__info--fullname">{fullname}</div>
+                    <div className="profile__wrapper__info--username">@{username}</div>
+                </div>
+                <div className="profile__wrapper--image">
+                    <img style={{"width": "90px", "height": "90px", "borderRadius": "50%"}} src={defaultProfile} alt="Profile Image" />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Profile;
